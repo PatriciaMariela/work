de040c9f9afdac78405e254cb69eb055e2ac02bd
Patricia Bernasconi <pachuberna@gmail.com>
2022-06-01 19:56:44 -0300

    Estructura principal

---
+++ nota/src/main/java/nota/Nota.java
@@ -0,0 +1,44 @@
+package nota;
+
+public class Nota {
+	
+	/**
+	 * pre: valorInicial esta comprendido entre 0 y 10.
+	 * post: inicializa la Nota con el valor indicado.
+	 */
+
+	public Nota(int valorInicial) {
+		
+	}
+
+	/**
+	 * post: devuelve el valor numerico de la Nota, comprendido entre 0 y 10.
+	 */
+	public int getValor() {
+		return 0;
+		
+	}
+	
+	/**
+	 * post: indica si la Nota permite o no la aprobacion.
+	 */
+	public boolean aprobado() {
+		return false;
+		
+	}
+	
+	/**
+	 * pre: nuevoValor esta comprendido entre 0 y 10.
+	 * post: modifica el valor numerico de la Nota, cambuandolo por nuevoValor, 
+	 * siempre y cuando nuevoValor sea superior al valor numerico actual de la Nota.
+	 */
+	public void recuperar(int nuevoValor) {
+		
+		
+	}
+	
+	private boolean esValorCorrecto(int x) {
+		return false;
+		
+	}
+}
